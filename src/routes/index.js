5fe3dbd0890601b4168faab7b6c4c1c456da2ea1
Debojyoti1915001 <debojyoti76@gmail.com>
2021-10-31 02:02:48 +0530

    Route For Contact

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,6 +15,9 @@ router.get('/', (req, res) => {
 router.get('/about', (req, res) => {
     res.render('./about')
 });
+router.get('/contact', (req, res) => {
+    res.render('./contact')
+});
 router.post('/contact',async(req,res)=>{
     try{
         const { name, email, subject, message } = req.body;
